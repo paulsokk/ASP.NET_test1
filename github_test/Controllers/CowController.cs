311921840608cb0b3b77a6ce1fde0dd4712f46fa
Paul <paulsokk@hotmail.com>
2016-08-26 10:44:08 +0300

    Added CowView, CowController and CowModel.

---
+++ github_test/Controllers/CowController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using github_test.Models;
+
+namespace github_test.Controllers
+{
+    public class CowController : Controller
+    {
+        // GET: Cow
+        public ActionResult Index()
+        {
+            CowModel Cow1 = new CowModel() { Name = "Alma", Weight = 120 };
+            return View("Cow name: {0}. Cow weight: {1}.", Cow1.Name, Cow1.Weight);
+        }
+    }
+}
---
+++ github_test/Models/CowModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace github_test.Models
+{
+    public class CowModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+    }
+}
